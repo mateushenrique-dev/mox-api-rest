45bd8807bb0138c31ffd4350c29db00578909aaf
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 17:54:03 +0200

    create router video

--- src/routes.ts
+++ src/routes.ts
@@ -1,16 +1,25 @@
 import { Router } from 'express';
 
+//Category
 import { CreateCategoryController } from './controllers/CreateCategoryController';
 import { GetAllCategoryController } from './controllers/GetAllCategoryController';
 import { DeleteCategoryController } from './controllers/DeleteCategoryController';
 import { UpdateCategoryController } from './controllers/UpdateCategoryController';
 
+//Video
+import { CreateVideoController } from './controllers/CreateVideoController';
+
 const routes = Router();
 
+//Category
 routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoryController().handle);
 routes.delete('/categories/:id', new DeleteCategoryController().handle);
 routes.put('/categories', new UpdateCategoryController().handle);
+
+//Video
+routes.post('/video', new CreateVideoController().handle);
+
 
 
 export { routes }
