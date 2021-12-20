67d21dc24858292862e2a25be7aaf5e6cc136d39
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 20:46:20 +0200

    update Gelt all Video  service

--- src/services/GetAllVideoService.ts
+++ src/services/GetAllVideoService.ts
@@ -1,11 +1,8 @@
-import { getRepository } from 'typeorm'
 import { Video } from '../entities/Video'
-
+import { repository } from '../repositories/videoRepository'
 
 class GetAllVideoService{
   async  execute():Promise<Video[]>{
-    const repository = getRepository(Video);
-
     const video = await repository.find(
       {
         relations: ['category']
