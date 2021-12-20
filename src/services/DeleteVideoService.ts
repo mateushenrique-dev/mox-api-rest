85881906a5aefcadf77f6fc606209963d2e6bf5d
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 19:32:56 +0200

    create delete service in video

---
+++ src/services/DeleteVideoService.ts
@@ -0,0 +1,17 @@
+import { getRepository } from 'typeorm';
+import { Video } from '../entities/Video';
+
+class DeleteVidioService{
+  async execute(id : string):Promise<void | Error>{
+    const repository = getRepository(Video);
+    const isExists = await repository.findOne(id);
+
+    if(!isExists){
+      return new Error('Video does not exists');
+    }
+
+    await repository.delete(id);
+  }
+}
+
+export { DeleteVidioService }
