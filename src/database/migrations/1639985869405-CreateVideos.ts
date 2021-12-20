98c538d9ccb0b6030afba40e321a3521f6f77859
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 09:38:49 +0200

    create migration of videos

---
+++ src/database/migrations/1639985869405-CreateVideos.ts
@@ -0,0 +1,11 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class CreateVideos1639985869405 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
