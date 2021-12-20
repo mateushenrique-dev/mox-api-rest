fc8c40e096d80d651b20b655d259752f934b0a12
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 14:53:04 +0200

    create getallcategoryController

---
+++ src/controllers/GetAllCategoryController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import { GetAllCategoryService } from '../services/GetAllCategoryService';
+
+class GetAllCategoryController{
+  async handle(request, response):Promise<Response>{
+    const service = new GetAllCategoryService();
+
+    const result = await  service.execute()
+
+    return response.json(result);
+  }
+}
+
+export  { GetAllCategoryController }
