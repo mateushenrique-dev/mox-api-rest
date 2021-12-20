d5253e09a37010c6cf2cdf62a85bbb65e687a1c4
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 13:10:52 +0200

    configure express of receive Jason request

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import './database';
 import { routes } from './routes'
 const app = express();
 
-
+app.use(express.json())
 app.use(routes);
 
 app.listen(3003, () => console.log('server running'));
