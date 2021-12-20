078321570d2eeb16c7a89d9c53de2376e70216c3
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 16:36:57 +0200

    add router update category

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from 'express';
-import { CreateCategoryController } from './controllers/CreateCategoryController'
-import { GetAllCategoryController } from './controllers/GetAllCategoryController'
-import { DeleteCategoryController } from './controllers/DeleteCategoryController'
+
+import { CreateCategoryController } from './controllers/CreateCategoryController';
+import { GetAllCategoryController } from './controllers/GetAllCategoryController';
+import { DeleteCategoryController } from './controllers/DeleteCategoryController';
+import { UpdateCategoryController } from './controllers/UpdateCategoryController';
+
 const routes = Router();
 
 routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoryController().handle);
 routes.delete('/categories/:id', new DeleteCategoryController().handle);
+routes.put('/categories', new UpdateCategoryController().handle);
 
 
 export { routes }
