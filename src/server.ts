f8b63274c8ed08552c654d63d2d0f7fbcfd2b258
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 12:42:37 +0200

    modifique routes

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
 import 'reflect-metadata'
 import express from 'express';
 import './database';
-
+import { routes } from './routes'
 const app = express();
 
+app.use(routes);
 app.listen(3003, () => console.log('server running'));
