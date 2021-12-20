71a844d51a529f3b5e1bba0cc529f1de9aa7dd90
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 16:06:50 +0200

    create router delete category

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { CreateCategoryController } from './controllers/CreateCategoryController'
 import { GetAllCategoryController } from './controllers/GetAllCategoryController'
+import { DeleteCategoryController } from './controllers/DeleteCategoryController'
 const routes = Router();
 
 routes.post('/categories', new CreateCategoryController().handle);
 routes.get('/categories', new GetAllCategoryController().handle);
+routes.delete('/categories/:id', new DeleteCategoryController().handle);
 
 
 export { routes }
