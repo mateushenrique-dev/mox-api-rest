89f0485dca6ba3f4e7c52b107bf837eb3225a734
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 17:24:35 +0200

    create servives video

---
+++ src/services/CreateCategoryVideo.ts
@@ -0,0 +1,31 @@
+import { getRepository } from 'typeorm'
+import { Category } from '../entities/Category';
+import { Video } from '../entities/Video';
+
+
+type videoRequest = {
+  name:string
+  duration:number
+  description:string
+  category_id:string
+}
+
+class CreateCategoryVideo{
+  async execute({name, duration, description, category_id }: videoRequest):Promise<Video | Error> {
+    const repositoryCategory = getRepository(Category);
+    const repository = getRepository(Video);
+
+    const isExists = await repositoryCategory.findOne(category_id);
+
+    if(!isExists){
+      return new Error('Category does not exists');
+    }
+
+    const video = repository.create({ name, duration, description, category_id });
+
+    await repository.save(video);
+    return video;
+  }
+}
+
+export { CreateCategoryVideo }
