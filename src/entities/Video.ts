974a8953616b789e7f084bd29c2112bb3d94dd28
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 11:58:05 +0200

    create entity video to reference the  table videos

---
+++ src/entities/Video.ts
@@ -0,0 +1,33 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn, ManyToOne, JoinColumn } from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import { Category } from './Category'
+
+@Entity('videos')
+class Video{
+  @PrimaryColumn()
+  id:string
+  
+  @Column()
+  name:string
+
+  @Column()
+  description:string
+
+  @Column()
+  category_id:string
+
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category:Category
+
+  @CreateDateColumn()
+  created_at:Date
+
+  constructor(){ 
+    if(!this.id){ 
+      this.id = uuid();
+    }
+  }
+}
+
+export { Video }
