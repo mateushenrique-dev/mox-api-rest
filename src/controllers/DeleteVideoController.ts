0e8fb1492b0f5ec4dd3191a8ac9fe198ad423034
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 19:41:12 +0200

    refectore import in archive DeleteVideoController

--- src/controllers/DeleteVideoController.ts
+++ src/controllers/DeleteVideoController.ts
@@ -1,10 +1,10 @@
-import { DeleteVideoController } from '../services/DeleteVideoController';
+import { DeleteVideoService } from '../services/DeleteVideoService';
 import { Request, Response } from 'express'
 class DeleteVideoController{
   async handle(request:Request, response:Response):Promise<Response | void >{
     const { id } = request.params
 
-    const service = new DeleteVideoController();
+    const service = new DeleteVideoService();
     const result = await service.execute(id);
 
     if(result instanceof Error){
