c8af892a173187fab4eb1c26ed24d06540293d69
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 21:01:27 +0200

    modifique create video  service

--- src/services/CreateVideoService.ts
+++ src/services/CreateVideoService.ts
@@ -1,13 +1,7 @@
 import { Video } from '../entities/Video';
 import { repository } from '../repositories/videoRepository';
 import { repository as repositoryCategory } from '../repositories/categoryRepository';
-
-type videoRequest = {
-  name:string
-  duration:number
-  description:string
-  category_id:string
-}
+import { videoRequest } from '../interfaces/videoInterface'
 
 class CreateVideoService{
   async execute({name, duration, description, category_id }: videoRequest):Promise<Video | Error> {
