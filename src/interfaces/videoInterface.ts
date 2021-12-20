114395dc696d7af84d327b9551478a659f288c7e
yazalde filimone pinto <yazaldefilimon@gmail.com>
2021-12-20 21:00:43 +0200

    create interface video

---
+++ src/interfaces/videoInterface.ts
@@ -0,0 +1,15 @@
+export type videoRequest = {
+  name:string
+  duration:number
+  description:string
+  category_id:string
+}
+
+export type VideoUpdateRequest = {
+  id:string
+  name:string
+  description:string
+  duration:number
+  category_id:string
+}
+
